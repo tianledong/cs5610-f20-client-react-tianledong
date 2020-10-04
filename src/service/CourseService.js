ee6084cbf6fc8ed70892a6b6d1a805fa0b36f88e
Tianle Dong <dtllawrence@gmail.com>
2020-10-03 23:42:31 -0400

    create CourseService.js

---
+++ src/service/CourseService.js
@@ -0,0 +1,37 @@
+const url = "https://wbdv-generic-server.herokuapp.com/api/tianledong/courses"
+
+export const findAllCourses = () =>
+    fetch(url)
+        .then(response => response.json())
+
+export const createCourse = (newCourse) =>
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(newCourse),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const deleteCourse = (courseId) =>
+    fetch(`${url}/${courseId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+
+export const updateCourse = (courseId, newCourse) =>
+    fetch(`${url}/${courseId}`, {
+        method: 'PUT',
+        body: JSON.stringify(newCourse),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const findCourseById = (courseId) =>
+    fetch(`${url}/${courseId}`, {
+        method: 'GET'
+    })
+        .then(response => response.json())
