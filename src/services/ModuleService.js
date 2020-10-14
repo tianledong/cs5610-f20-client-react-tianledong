bf06487f35b90d0e0e3a1fac38cfae05f43911c8
Tianle Dong <dtllawrence@gmail.com>
2020-10-14 13:47:37 -0400

    finished ModuleService.js

---
+++ src/services/ModuleService.js
@@ -0,0 +1,32 @@
+const courseUrl = "https://wbdv-generic-server.herokuapp.com/api/tianledong/courses"
+const moduleUrl = "https://wbdv-generic-server.herokuapp.com/api/tianledong/modules"
+
+const createModuleForCourse = (courseId, newModule) =>
+    fetch(`${courseUrl}/${courseId}/modules`, {
+        method: "POST",
+        body: JSON.stringify(newModule),
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then(response => response.json());
+
+const updateModule = (moduleId, newModule) =>
+    fetch(`${moduleUrl}/${moduleId}`, {
+        method: "PUT",
+        body: JSON.stringify(newModule),
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then(response => response.json());
+
+const deleteModule = (moduleId) =>
+    fetch(`${moduleUrl}/${moduleId}`, {
+        method: "DELETE"
+    }).then(response => response.json());
+
+const findModulesForCourse = (coueseId) =>
+    fetch(`${courseUrl}/${coueseId}/modules`)
+        .then(response => response.json());
+
+export default {createModuleForCourse, updateModule, deleteModule, findModulesForCourse};
+
