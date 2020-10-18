edf5c252cf7fb3fe430d9bff8b5c84c8b1ecbc96
Tianle Dong <dtllawrence@gmail.com>
2020-10-18 15:57:45 -0400

    finished lessonActions.js

---
+++ src/actions/lessonActions.js
@@ -0,0 +1,26 @@
+export const CREATE_LESSON = "CREATE_LESSON";
+export const UPDATE_LESSON = "UPDATE_LESSON";
+export const DELETE_LESSON = "DELETE_LESSON";
+export const FIND_LESSONS_FOR_MODULE = "FIND_LESSONS_FOR_MODULE";
+
+export const updateLesson = (newLesson) => ({
+    type: UPDATE_LESSON,
+    lesson: newLesson
+})
+
+export const createLesson = (newLesson) => ({
+    type: CREATE_LESSON,
+    lesson: newLesson
+})
+
+export const deleteLesson = (lessonId) => ({
+    type: DELETE_LESSON,
+    lessonId: lessonId
+})
+
+export const findModulesForCourse = (lessons) => ({
+    type: FIND_LESSONS_FOR_MODULE,
+    lessons: lessons
+})
+
+
--- src/actions/moduleActions.js
+++ src/actions/moduleActions.js
@@ -1,7 +1,7 @@
 export const CREATE_MODULE = "CREATE_MODULE";
 export const UPDATE_MODULE = "UPDATE_MODULE";
 export const DELETE_MODULE = "DELETE_MODULE";
-export const FIND_Modules_For_Course = "FIND_Modules_For_Course";
+export const FIND_MODULE_FOR_COURSE = "FIND_Modules_For_Course";
 
 export const updateModule = (newModule) => ({
     type: UPDATE_MODULE,
@@ -19,7 +19,7 @@ export const deleteModule = (moduleId) => ({
 })
 
 export const findModulesForCourse = (modules) => ({
-    type: FIND_Modules_For_Course,
+    type: FIND_MODULE_FOR_COURSE,
     modules: modules
 })
 
