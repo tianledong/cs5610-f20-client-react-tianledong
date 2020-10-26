de9c9cf4e0ae2685d83e0dc7b40255fe8b22ad16
Tianle Dong <dtllawrence@gmail.com>
2020-10-26 09:33:33 -0400

    create WidgetService.js

---
+++ src/services/WidgetService.js
@@ -0,0 +1,36 @@
+const topicUrl = "https://whiteboard-f20.herokuapp.com/api/topics"
+const widgetUrl = "https://whiteboard-f20.herokuapp.com/api/widgets";
+
+export const findWidgetsForTopic = (topicId) =>
+    fetch(`${topicUrl}/${topicId}/widgets`)
+        .then(response => response.json())
+
+export const createWidgetForTopic = (topicId, widget) =>
+    fetch(`${topicUrl}/${topicId}/widgets`, {
+        method: "POST",
+        body: JSON.stringify(widget),
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then(response => response.json())
+
+export const updateWidget = (widgetId, widget) =>
+    fetch(`${widgetUrl}/${widgetId}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            "content-type": "application/json"
+        }
+    }).then(response => response.json())
+
+export const deleteWidget = (widgetId) =>
+    fetch(`${widgetUrl}/${widgetId}`, {
+        method: "DELETE"
+    }).then(response => response.json())
+
+export default {
+    findWidgetsForTopic,
+    createWidgetForTopic,
+    updateWidget,
+    deleteWidget
+}
