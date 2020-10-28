484f4868f607b1585ebd718c0164c588ca045bdd
Tianle Dong <dtllawrence@gmail.com>
2020-10-28 02:07:33 -0400

    add up&down function

--- src/reducers/widgetReducer.js
+++ src/reducers/widgetReducer.js
@@ -3,6 +3,11 @@ import 'array.prototype.move';
 const initialState = {
     widgets: []
 };
+const swapOrder = (from, to, widgets) => {
+    let newWidgets = [...widgets];
+    newWidgets[from] = newWidgets.splice(to, 1, newWidgets[from])[0];
+    return newWidgets;
+}
 
 export const widgetReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -32,9 +37,11 @@ export const widgetReducer = (state = initialState, action) => {
                     widget => widget.id === action.widget.id ? action.widget : widget)
             }
         case "MOVE_WIDGET":
+            const newWidgets = swapOrder(action.from, action.to, state.widgets);
+            console.log(newWidgets);
             return {
                 ...state,
-                widgets: state.widgets.move(action.from, action.to)
+                widgets: newWidgets
             }
         default:
             return state
